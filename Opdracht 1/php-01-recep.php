1c92915c9205b135173599cb08412b3626341f72
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-09-12 13:50:07 +0200

    Opdracht 1 gemaakt

---
+++ Opdracht 1/php-01-recep.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 1 | Recep</title>
+</head>
+
+<body>
+    
+    <form action="action_page.php" method="get">
+        <h4> Enter your name:</h4>
+    <input type="text" id="name" name="name">
+
+    <?php
+
+    ?>
+    
+</body>
+
+</html>
