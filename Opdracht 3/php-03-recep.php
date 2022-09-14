fc6761acfd9b31cdf6e12977edc919abd16934e3
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-09-14 09:13:08 +0200

    o3 gemaakt en 4 ready gezet

---
+++ Opdracht 3/php-03-recep.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 3 | Recep</title>
+</head>
+<style>
+
+    body{
+        color: red;
+        font-weight: bold;
+    }
+
+</style>
+
+<body>
+   
+    <?php 
+
+        for ($i = 1; $i < 21; $i++) {
+            echo $i . ' ';
+        }
+
+        echo "<br>";
+
+
+                $c = 1;
+        while ( $c < 21 ) {
+            echo $c . ' ';
+            $c++;
+        }
+        
+
+    ?>
+
+</body>
+
+</html>
