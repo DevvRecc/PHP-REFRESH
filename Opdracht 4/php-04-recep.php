825b298bab4566e5d2c35a50755e39cc59c7563b
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-09-14 09:23:34 +0200

    Digitale klok zetten o4 af

--- Opdracht 4/php-04-recep.php
+++ Opdracht 4/php-04-recep.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Opdracht 4 | Recep</title>
 </head>
+
 <body>
-    
+    <?php
+    date_default_timezone_set('Europe/Amsterdam');
+    echo  '<h3>'. 'Mijn Klok ' . '</h3>' . date('h:i:sa');
+    ?>
 </body>
+
 </html>
